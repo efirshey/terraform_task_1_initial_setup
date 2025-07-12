a19ac76433f6ac1e732dd45940ee7b727761fbd0
Vicheslav Der <efirshey@gmail.com>
2025-07-12 19:57:11 +0300

    namechange

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -12,13 +12,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "efirshey-resources"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.amazing_name
+  resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
